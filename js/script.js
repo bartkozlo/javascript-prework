5e51c8cb2eedc960efe019257956d207291ea811
Bartosz Kozłowski <bartkozlo@gmail.com>
2021-07-01 20:06:12 +0200

    Add moves for player

--- js/script.js
+++ js/script.js
@@ -17,3 +17,23 @@ else if(randomNumber == 3){
 }
 
 printMessage('I choose: ' + computerMove);
+
+let playerInput = prompt('Your turn! 1: stone, 2: paper, 3: scissors.');
+
+console.log('Player choose: ' + playerInput);
+
+let playerMove = 'unknown choose';
+
+if(playerInput == '1'){
+  playerMove = 'stone';
+}
+
+else if(playerInput == '2'){
+  playerMove = 'paper';
+}
+
+else if(playerInput == '3'){
+  playerMove = 'scissors';
+}
+
+printMessage('Your choose: ' + playerMove);
