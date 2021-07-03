068b9cd456357403b53478582353655724ab7991
Bartosz Kozłowski <bartkozlo@gmail.com>
2021-07-03 16:41:12 +0200

    Add buttons in html, function playGame and listener

--- js/script.js
+++ js/script.js
@@ -1,57 +1,77 @@
-function getMoveName(argMoveId){
-  if(argMoveId == 1){
-    return 'stone';
-  } else if(argMoveId == 2){
-    return 'paper';
-  } else if(argMoveId == 3){
-    return 'scissors';
+function playGame(playerInput){
+
+  clearMessages();
+
+  function getMoveName(argMoveId){
+    if(argMoveId == 1){
+      return 'stone';
+    } else if(argMoveId == 2){
+      return 'paper';
+    } else if(argMoveId == 3){
+      return 'scissors';
+    }
+    printMessage('Unknown move with id ' + argMoveId + '.');
+    return 'unknown choose';
   }
-  printMessage('Unknown move with id ' + argMoveId + '.');
-  return 'unknown choose';
+
+  playerMove = getMoveName(playerInput);
+
+  let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+  console.log('The drawn number is: ' + randomNumber);
+
+  let argComputerMove = getMoveName(randomNumber);
+
+  printMessage('I choose: ' + argComputerMove);
+
+  console.log('Player choose: ' + playerInput);
+
+  let argPlayerMove = getMoveName(playerInput);
+
+  printMessage('Your choose: ' + argPlayerMove);
+
+  displayResult(argComputerMove, argPlayerMove);
+
+  function displayResult(argComputerMove, argPlayerMove){
+    if(argComputerMove == 'stone' && argPlayerMove == 'paper'){
+      printMessage('You win!');
+    } else if(argComputerMove == 'stone' && argPlayerMove == 'stone'){
+      printMessage("It's a draw!");
+    } else if(argComputerMove == 'stone' && argPlayerMove == 'scissors'){
+      printMessage('You lose!');
+    } else if(argComputerMove == 'paper' && argPlayerMove == 'stone'){
+      printMessage('You lose!');
+    } else if(argComputerMove == 'paper' && argPlayerMove == 'paper'){
+      printMessage("It's a draw!");
+    } else if(argComputerMove == 'paper' && argPlayerMove == 'scissors'){
+      printMessage('You win!');
+    } else if(argComputerMove == 'scissors' && argPlayerMove == 'stone'){
+      printMessage('You win!');
+    } else if(argComputerMove == 'scissors' && argPlayerMove == 'scissors'){
+      printMessage("It's a draw!");
+    } else if(argComputerMove == 'scissors' && argPlayerMove == 'paper'){
+      printMessage('You lose!');
+    } else if(argComputerMove == 'stone' && argPlayerMove == 'unknown choose'){
+      printMessage('You have to pick number from 1 to 3!');
+    } else if(argComputerMove == 'paper' && argPlayerMove == 'unknown choose'){
+      printMessage('You have to pick number from 1 to 3!');
+    } else if(argComputerMove == 'scissors' && argPlayerMove == 'unknown choose'){
+      printMessage('You have to pick number from 1 to 3!');
+    }
+  }
 }
 
-function displayResult(argComputerMove, argPlayerMove){
-  if(argComputerMove == 'stone' && argPlayerMove == 'paper'){
-    printMessage('You win!');
-  } else if(argComputerMove == 'stone' && argPlayerMove == 'stone'){
-    printMessage("It's a draw!");
-  } else if(argComputerMove == 'stone' && argPlayerMove == 'scissors'){
-    printMessage('You lose!');
-  } else if(argComputerMove == 'paper' && argPlayerMove == 'stone'){
-    printMessage('You lose!');
-  } else if(argComputerMove == 'paper' && argPlayerMove == 'paper'){
-    printMessage("It's a draw!");
-  } else if(argComputerMove == 'paper' && argPlayerMove == 'scissors'){
-    printMessage('You win!');
-  } else if(argComputerMove == 'scissors' && argPlayerMove == 'stone'){
-    printMessage('You win!');
-  } else if(argComputerMove == 'scissors' && argPlayerMove == 'scissors'){
-    printMessage("It's a draw!");
-  } else if(argComputerMove == 'scissors' && argPlayerMove == 'paper'){
-    printMessage('You lose!');
-  } else if(argComputerMove == 'stone' && argPlayerMove == 'unknown choose'){
-    printMessage('You have to pick number from 1 to 3!');
-  } else if(argComputerMove == 'paper' && argPlayerMove == 'unknown choose'){
-    printMessage('You have to pick number from 1 to 3!');
-  } else if(argComputerMove == 'scissors' && argPlayerMove == 'unknown choose'){
-    printMessage('You have to pick number from 1 to 3!');
-  }
-}
+document.getElementById('play-stone').addEventListener('click', function(){
+  playGame('1');
+});
 
-let randomNumber = Math.floor(Math.random() * 3 + 1);
+document.getElementById('play-paper').addEventListener('click', function(){
+  playGame('2');
+});
 
-console.log('The drawn number is: ' + randomNumber);
+document.getElementById('play-scissors').addEventListener('click', function(){
+  playGame('3');
+});
 
-let argComputerMove = getMoveName(randomNumber);
 
-printMessage('I choose: ' + argComputerMove);
 
-let playerInput = prompt('Your turn! 1: stone, 2: paper, 3: scissors.');
-
-console.log('Player choose: ' + playerInput);
-
-let argPlayerMove = getMoveName(playerInput);
-
-printMessage('Your choose: ' + argPlayerMove);
-
-let result = displayResult(argComputerMove, argPlayerMove);
